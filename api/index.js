3d67bb4c774fa8447dd759e36dbe7a905a5b26d1
Fran <franmartinez26@gmail.com>
2024-04-17 01:10:11 +0200

    prueba

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
-app.get('/inserta', (req, res) => {
-    const htmlContent = `
-      <div id="miContenido">
-        <button id="miBoton">Haz clic aquí</button>
-        <p>Este es un texto de ejemplo que será insertado en la página.</p>
-      </div>
-      <script>
-        document.getElementById('miBoton').addEventListener('click', function() {
-          alert('¡Botón presionado!');
-        });
-      </script>
-    `;
-    res.send(htmlContent);
-  });
-  
+// No necesitas requerir 'path' para definir rutas en Express
+
+app.get('/inserta', (req, res) => { // Usa una cadena de texto para la ruta
+  const htmlContent = `
+    <div id="miContenido">
+      <button id="miBoton">Haz clic aquí</button>
+      <p>Este es un texto de ejemplo que será insertado en la página.</p>
+    </div>
+    <script>
+      document.getElementById('miBoton').addEventListener('click', function() {
+        alert('¡Botón presionado!');
+      });
+    </script>
+  `;
+  res.send(htmlContent);
+});
