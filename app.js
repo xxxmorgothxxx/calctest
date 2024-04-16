1ab27f833e68a20918e900fa67db54bf6ec39cee
Fran <franmartinez26@gmail.com>
2024-04-17 00:11:13 +0200

    prueba

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 app.get('/', (req, res) => {
     //res.send("Hola desde la API");
-    res.sendFile(path.join(__dirname + "api/index.html"));
+    res.sendFile(path.join(__dirname + "(/api/index.html"));
 });
 
 app.listen(port, () => {
@@ -20,6 +20,12 @@ app.listen(port, () => {
 
 const cors = require('cors');
 app.use(cors({
-  origin: "https://https://cbc43b-2.myshopify.com/",
-  credentials: true,
+  origin: "https://https://cbc43b-2.myshopify.com/"
 }));
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "https://cbc43b-2.myshopify.com"); // Permite el dominio de Shopify
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
