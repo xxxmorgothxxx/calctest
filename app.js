fd483f7b5150a4416d75ddf2a6aa95dfb6c71b16
Fran <franmartinez26@gmail.com>
2024-04-17 00:39:06 +0200

    prueba

--- app.js
+++ app.js
@@ -4,24 +4,30 @@ const port = process.env.PORT || 3000;
 
 const path = require('path');
 
-const cors = require("cors");
+const cors = require('cors');
 
 app.get('/', (req, res) => {
-    //res.send("Hola desde la API");
-    res.sendFile(path.join(__dirname + "/api/index.html"));
+  //res.send("Hola desde la API");
+  res.sendFile(path.join(__dirname + "/api/index.html"));
 });
 
 app.listen(port, () => {
   console.log(`API escuchando en puerto: ${port}`);
 });
 
-app.use(cors({
+/* app.use(cors({
   origin: "https://cbc43b-2.myshopify.com",
   credentials: true
-}));
+})); */
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+};
 
-/* const whitelist = ["https://cbc43b-2.myshopify.com/"];
+const domainsFromEnv = process.env.CORS_DOMAINS || "";
+
+const whitelist = domainsFromEnv.split(",").map(item => item.trim());
+
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
@@ -33,4 +39,4 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions)); */
+app.use(cors(corsOptions));
