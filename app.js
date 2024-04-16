8505e0d5c7efa6cd1713081542ae3ceca8cda04f
Fran <franmartinez26@gmail.com>
2024-04-17 00:22:53 +0200

    prueba

--- app.js
+++ app.js
@@ -24,7 +24,18 @@ app.use(cors({
 })); */
 
 
-import express from "express"
-import cors from "cors"
-
-app.use(cors())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
