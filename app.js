ef4d453470fa98f4e99cdf1b536af9cfa0675311
Fran <franmartinez26@gmail.com>
2024-04-16 23:01:51 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require('path');
+
+
+/* app.get('/', (req, res) => {
+  res.json({ mensaje: '¡Hola desde mi API!' });
+}); */
+
+app.get('/', (req, res) => {
+    //res.send("Hola desde la API");
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`API escuchando en puerto: ${port}`);
+});
+
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://https://cbc43b-2.myshopify.com/'
+}));
