f7243887619f552129af99372036a64d9fc7b99e
Fran <franmartinez26@gmail.com>
2024-04-17 00:44:20 +0200

    prueba

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const path = require('path');
-
-const cors = require('cors');
-
-app.get('/', (req, res) => {
-  //res.send("Hola desde la API");
-  res.sendFile(path.join(__dirname + "/api/index.html"));
-});
-
-app.listen(port, () => {
-  console.log(`API escuchando en puerto: ${port}`);
-});
-
-/* app.use(cors({
-  origin: "https://cbc43b-2.myshopify.com",
-  credentials: true
-})); */
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config()
-};
-
+// Configuración de CORS
 const domainsFromEnv = process.env.CORS_DOMAINS || "";
-
 const whitelist = domainsFromEnv.split(",").map(item => item.trim());
-
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
@@ -40,3 +24,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + "/api/index.html"));
+});
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`API escuchando en puerto: ${port}`);
+});
